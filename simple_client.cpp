8238567c505986c2f8566ae5680502d2313e6659
angelos <angelos.plastropoulos@innotecuk.com>
2019-08-05 15:31:50 +0100

    ## 0.0.3 (complementary)

--- simple_client.cpp
+++ simple_client.cpp
@@ -1,22 +1,33 @@
+/*
+    TCP client
+    compile:
+    g++ -o simple_client simple_client.cpp -lboost_system -lboost_thread -std=c++11
+
+ */
 #include <iostream>
 #include <string>
+#include <stdlib.h>
 
 #include <boost/asio.hpp>
 
 using boost::asio::ip::tcp;
 
-int main()
+int main(int argc, char* argv[])
 {
     boost::asio::io_service io_service;
 
     tcp::socket socket(io_service);
 
+    if (argc < 2) 
+    {   
+        std::cout << "Use it properly!" << std::endl;
+        return 1;
+    }
+
     socket.connect(tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 13));
-
-    const std::string msg = "Hello from client!\n";
+    
     boost::system::error_code error;
-
-    boost::asio::write(socket, boost::asio::buffer(msg), error);
+    boost::asio::write(socket, boost::asio::buffer(std::string(argv[1])), error);
 
     if (!error)
     {
@@ -26,7 +37,7 @@ int main()
         std::cerr << "sent failed because of " << error.message() << std::endl;
     }
     
-
+    std::cout << "waiting for reply" << std::endl;
     boost::asio::streambuf receive_buffer;
     boost::asio::read(socket, receive_buffer, boost::asio::transfer_all(), error);
 
@@ -38,5 +49,7 @@ int main()
         const char* data = boost::asio::buffer_cast<const char*>(receive_buffer.data());  
         std::cout << data << std::endl; 
     }
+    std::cout << "finished" << std::endl;
+
     return 0;
 }
